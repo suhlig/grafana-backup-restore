4eeb3812d56708761b1074b0328b1ed2f9d0c361
Steffen Uhlig <steffen@familie-uhlig.net>
2021-08-01 21:05:00 +0200

    Add simplest acceptance test

---
+++ acceptance-tests/dashboards_test.go
@@ -0,0 +1,12 @@
+package acceptance_tests_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Backup and restore dashboards", func() {
+	It("works", func() {
+		Expect(true).ToNot(BeFalse())
+	})
+})
